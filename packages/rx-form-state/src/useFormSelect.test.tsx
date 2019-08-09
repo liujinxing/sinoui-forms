c3b06572439a8e4adadc535967e82be54d047cab
刘进行 <jackingstarliu@gmail.com>
2019-08-09 16:28:24 +0800

    chore: useFormSelect支持从指定的formState中获取状态

--- packages/rx-form-state/src/useFormSelect.test.tsx
+++ packages/rx-form-state/src/useFormSelect.test.tsx
@@ -3,6 +3,7 @@ import { renderHook } from '@testing-library/react-hooks';
 import useFormState from './useFormState';
 import FormStateContext from './FormStateContext';
 import useFormSelect from './useFormSelect';
+import createFormState from './createFormState';
 
 it('获取表单状态', () => {
   const Wrapper = ({ children }: { children?: React.ReactNode }) => {
@@ -20,6 +21,19 @@ it('获取表单状态', () => {
   const { result } = renderHook(
     () => useFormSelect((formStateModel) => formStateModel.values),
     { wrapper: Wrapper },
+  );
+
+  expect(result.current).toEqual({
+    userName: '张三',
+  });
+});
+
+it('从指定的formState中获取状态', () => {
+  const formState = createFormState({
+    userName: '张三',
+  });
+  const { result } = renderHook(() =>
+    useFormSelect(formState, (formStateModel) => formStateModel.values),
   );
 
   expect(result.current).toEqual({
